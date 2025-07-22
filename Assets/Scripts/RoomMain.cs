d725421f6a067f2fda71aa8001a87b64ee120e2b
smilejsu82 <smilejsu82@gmail.com>
2025-07-22 13:03:05 +0900

    빠른 입장
    빠른 입장

--- Assets/Scripts/RoomMain.cs
+++ Assets/Scripts/RoomMain.cs
@@ -33,15 +33,21 @@ public class RoomMain : MonoBehaviourPunCallbacks
     
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
+        //무조건 손님임 
+        player2NicknameText.text = newPlayer.NickName;
         Debug.Log($"[RoomMain] 다른 플레이어가 룸에 입장 했습니다. : {newPlayer}");
     }
 
     public override void OnJoinedRoom()
     {
+        //무조건 나임 
         Debug.Log($"[RoomMain] 방에 입장했습니다.");
-        
         Debug.Log($"방이름 : {PhotonNetwork.CurrentRoom.Name}");
         Debug.Log($"방에있는 사람들 : {PhotonNetwork.PlayerList.Length}");
         Debug.Log($"내가({PhotonNetwork.LocalPlayer.NickName}) 방장인가? {PhotonNetwork.IsMasterClient}");
+
+        player1NicknameText.text = PhotonNetwork.MasterClient.NickName;
+        player2NicknameText.text = PhotonNetwork.LocalPlayer.NickName;
+        
     }
 }
